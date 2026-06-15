c8e734fcb71e98ccae7fd8fc98c8a5601a593bae
unknown <Gialinh0167@gmail.com>
2026-06-15 22:07:38 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://project.dangkhoa3ln.com")
+        policy.WithOrigins("https://project.dangkhoa3ln.com", "http://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
